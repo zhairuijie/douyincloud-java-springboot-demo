1a86c803c0d940e25c4425431c4cc542dc186349
liyi.ley <liyi.ley@bytedance.com>
2024-07-24 21:03:15 +0800

    add line
    Change-Id: I0e2c768ed5e44e99a4ecaff548d5afca6ff9de63

--- src/main/java/com/bytedance/douyinclouddemo/controller/HelloController.java
+++ src/main/java/com/bytedance/douyinclouddemo/controller/HelloController.java
@@ -28,6 +28,7 @@ public class HelloController {
     public JsonResponse hello(@RequestParam(value = "target", defaultValue = "mongodb") String target) {
         JsonResponse response = new JsonResponse();
         try {
+            System.out.println("增加了一行");
             HelloService helloService = factory.getHelloService(target);
             response.success("env:" + envMark + " hello " +  helloService.hello(target));
         }catch (Exception e){
