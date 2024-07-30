c21c2b27ee81e32b6c03fc11da1fddcac56af169
liyi.ley <liyi.ley@bytedance.com>
2024-07-30 14:53:15 +0800

    add line
    Change-Id: I7e321feb3e8acb42b903f897e44e301e16118ee1

--- src/main/java/com/bytedance/douyinclouddemo/controller/HelloController.java
+++ src/main/java/com/bytedance/douyinclouddemo/controller/HelloController.java
@@ -29,6 +29,7 @@ public class HelloController {
         JsonResponse response = new JsonResponse();
         try {
             System.out.println("部署2：增加了一行");
+            System.out.println("部署3：增加了一行");
             HelloService helloService = factory.getHelloService(target);
             response.success("env:" + envMark + " hello " +  helloService.hello(target));
         }catch (Exception e){
